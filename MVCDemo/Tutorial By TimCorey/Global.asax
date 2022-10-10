11e39e8491c572015ad169f1d54e3f02f82fbb16
Cha-er-si <vpcharles31@gmail.com>
2022-10-10 17:50:26 +0800

    People View and Controller

---
+++ MVCDemo/Tutorial By TimCorey/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tutorial_By_TimCorey.MvcApplication" Language="C#" %>
---
+++ MVCDemo/Tutorial By TimCorey/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Web;
+
+namespace Tutorial_By_TimCorey.Models
+{
+    public class PersonModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; } = 0;
+        public bool isALive { get; set; } = true;
+    }
+}

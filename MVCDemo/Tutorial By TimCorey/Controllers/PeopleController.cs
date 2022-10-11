d5ab4a65856e5b7a1c867000c95d6bbb356c19bc
Cha-er-si <vpcharles31@gmail.com>
2022-10-11 08:45:18 +0800

    Tutorial Done

--- MVCDemo/Tutorial By TimCorey/Controllers/PeopleController.cs
+++ MVCDemo/Tutorial By TimCorey/Controllers/PeopleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tutorial_By_TimCorey.Models;
 
 namespace Tutorial_By_TimCorey.Controllers
 {
@@ -11,7 +12,20 @@ namespace Tutorial_By_TimCorey.Controllers
         // GET: People
         public ActionResult Index()
         {
+            PersonModel person = new PersonModel();
+            int i = 5 / person.age;
             return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { firstName = "Prince", lastName = "Charles", age = 22 });
+            people.Add(new PersonModel { firstName = "Jr", lastName = "Bagaporo", age = 21 });
+            people.Add(new PersonModel { firstName = "John", lastName = "Doe", age = 34 });
+
+            return View(people);
         }
     }
 }
